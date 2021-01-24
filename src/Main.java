993769c819544acd35457a7c8b7bd96151646334
eren doganay <erendoganay60@hotmail.com>
2021-01-24 22:08:51 +0300

    commited exceptions from demo branch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+        Different.selam();
         System.out.println("ATATÜRK Havalimanına hosgeldiniz***** ");
         String sartlar="Yurtdisina cikis kuralları... \n"
                 + "1- Herhangi bir siyasi yasaginizin bulunmaması gerekmektedir. \n"
